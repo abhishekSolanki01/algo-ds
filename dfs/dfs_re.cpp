904d351ef9da1dd1256bbdf9d02501280de50994
rohan talwar <talwarrohan@yahoo.in>
2018-07-31 11:35:09 +0530

    Create dfs_re.cpp
    solve dfs using recursion

---
+++ dfs/dfs_re.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs(vector <long int >v[],int vi[],long int sn)
+{
+	vi[sn]=1;
+	for(i=0;i<v[sn].size();i++)
+	  {
+	  	 if(vi[v[i][sn]]==0)
+	  	   {
+	  	   	  dfs(v,vi,v[i][sn]);
+	  	   }
+	  }
+}
+
+
+int main() {
+    int n,m,x,y;
+    cin>>n>>m;
+    vector <long int >v[n+1];
+    while(m--)
+      {
+      	  cin>>x>>y;
+      	  v[x].push_back(y);
+      	  v[y].push_back(x);
+      }
+     int visted[n+1] 
+    for(i=1;i<=n;i++)
+      {
+      	vi[i]=0
+      }
+     long int sn; 
+      cin>>sn;
+    dfs(v,vi,sn);  
+	return 0;
+}
