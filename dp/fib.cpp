5348d9ab87b1ad025a98f7d91ba43f85ab6080ea
rohan talwar <talwarrohan@yahoo.in>
2018-08-01 14:36:04 +0530

    Create fib.cpp
    fibonacci

---
+++ dp/fib.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+   long int n,i;
+   cin>>n;
+   long long int dp[n+1];
+   dp[0]=0;
+   dp[1]=1;
+    for(i=2;i<=n;i++)
+      {
+        dp[i]=dp[i-1]+dp[i-2];	
+      }
+   cout<<dp[n]<<'\n';   
+   return 0;		
+}
